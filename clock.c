c2d9d2e7d620edc16d3a1632413333359da7ceda
Anders Selhammer <anders.selhammer@est.tech>
2018-03-29 13:22:13 +0200

    clock: Added pm timer

--- clock.c
+++ clock.c
@@ -124,6 +124,7 @@ struct clock {
 	LIST_HEAD(clock_subscribers_head, clock_subscriber) subscribers;
 	/* performance monitoring */
 	int performance_monitoring;
+	int pm_fd;
 	struct clock_pm_stats pm_stats_record;
 	struct clock_pm_record_list pm_recordlist;
 };
@@ -149,6 +150,16 @@ static int cid_eq(struct ClockIdentity *a, struct ClockIdentity *b)
 int clock_performance_monitoring(struct clock *c)
 {
 	return c->performance_monitoring;
+}
+
+static int clock_set_pm_timer(struct clock *c)
+{
+	return set_tmo_lin(c->pm_fd, PM_15M_TIMER);
+}
+
+static int clock_disarm_pm_timer(int fd)
+{
+	return set_tmo_lin(fd, 0);
 }
 
 static void clock_set_pmtime(struct clock *c)
@@ -326,6 +337,8 @@ void clock_destroy(struct clock *c)
 		clock_remove_port(c, p);
 	}
 	port_close(c->uds_port);
+	clock_disarm_pm_timer(c->pm_fd);
+	close(c->pm_fd);
 	free(c->pollfd);
 	if (c->clkid != CLOCK_REALTIME) {
 		phc_close(c->clkid);
@@ -1168,6 +1181,11 @@ struct clock *clock_create(enum clock_type type, struct config *config,
 		return NULL;
 	}
 
+	c->pm_fd = timerfd_create(CLOCK_MONOTONIC, 0);
+	if (c->pm_fd < 0) {
+		pr_err("timerfd_create failed: %m");
+		return NULL;
+	}
 	if (pm_create_clock_stats(&c->pm_stats_record)) {
 		pr_err("failed to create pm clock stats");
 		return NULL;
@@ -1198,6 +1216,7 @@ struct clock *clock_create(enum clock_type type, struct config *config,
 	port_dispatch(c->uds_port, EV_INITIALIZE, 0);
 
 	if (c->performance_monitoring) {
+		clock_set_pm_timer(c);
 		clock_set_pmtime(c);
 	}
 
@@ -1265,9 +1284,10 @@ static int clock_resize_pollfd(struct clock *c, int new_nports)
 {
 	struct pollfd *new_pollfd;
 
-	/* Need to allocate one whole extra block of fds for UDS. */
+	/* Need to allocate one extra fd for pm and one whole extra block
+	 * of fds for UDS. */
 	new_pollfd = realloc(c->pollfd,
-			     (new_nports + 1) * N_CLOCK_PFD *
+			     (1 + (new_nports + 1) * N_CLOCK_PFD) *
 			     sizeof(struct pollfd));
 	if (!new_pollfd) {
 		return -1;
@@ -1276,7 +1296,7 @@ static int clock_resize_pollfd(struct clock *c, int new_nports)
 	return 0;
 }
 
-static void clock_fill_pollfd(struct pollfd *dest, struct port *p)
+static void clock_fill_port_pollfd(struct pollfd *dest, struct port *p)
 {
 	struct fdarray *fda;
 	int i;
@@ -1290,6 +1310,12 @@ static void clock_fill_pollfd(struct pollfd *dest, struct port *p)
 	dest[i].events = POLLIN|POLLPRI;
 }
 
+static void clock_fill_pm_pollfd(struct pollfd *dest, struct clock *c)
+{
+	dest[0].fd = c->pm_fd;
+	dest[0].events = POLLIN|POLLPRI;
+}
+
 static void clock_check_pollfd(struct clock *c)
 {
 	struct port *p;
@@ -1299,10 +1325,12 @@ static void clock_check_pollfd(struct clock *c)
 		return;
 	}
 	LIST_FOREACH(p, &c->ports, list) {
-		clock_fill_pollfd(dest, p);
+		clock_fill_port_pollfd(dest, p);
 		dest += N_CLOCK_PFD;
 	}
-	clock_fill_pollfd(dest, c->uds_port);
+	clock_fill_port_pollfd(dest, c->uds_port);
+	dest += N_CLOCK_PFD;
+	clock_fill_pm_pollfd(dest, c);
 	c->pollfd_valid = 1;
 }
 
@@ -1541,7 +1569,7 @@ int clock_poll(struct clock *c)
 	struct port *p;
 
 	clock_check_pollfd(c);
-	cnt = poll(c->pollfd, (c->nports + 1) * N_CLOCK_PFD, -1);
+	cnt = poll(c->pollfd, 1 + (c->nports + 1) * N_CLOCK_PFD, -1);
 	if (cnt < 0) {
 		if (EINTR == errno) {
 			return 0;
@@ -1596,6 +1624,12 @@ int clock_poll(struct clock *c)
 			if (EV_STATE_DECISION_EVENT == event)
 				c->sde = 1;
 		}
+	}
+	cur += N_CLOCK_PFD;
+
+	/* Check the pm timer. */
+	if (cur[0].revents & (POLLIN|POLLPRI)) {
+		;
 	}
 
 	if (c->sde) {
